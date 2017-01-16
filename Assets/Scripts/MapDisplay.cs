e3b408fbc70876f0706a3427a18aa8d581d033dc
Patrik Durasek <pxd7081@rit.edu>
2017-01-16 20:47:22 +0100

    Perlin Noise Generation
    Map generation using Perlin Noise

---
+++ Assets/Scripts/MapDisplay.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class MapDisplay : MonoBehaviour
+{
+    public Renderer textureRender;
+
+    public void DrawNoiseMap(float[,] noiseMap)
+    {
+        int width = noiseMap.GetLength(0);
+        int height = noiseMap.GetLength(1);
+
+        Texture2D texture = new Texture2D(width, height);
+
+        Color[] colorMap = new Color[width*height];
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                colorMap[y * width + x] = Color.Lerp(Color.black, Color.white, noiseMap[x,y]);
+            }
+        }
+
+        texture.SetPixels(colorMap);
+        texture.Apply();
+
+        //Using shared material to avoid having to run the game to see the edit preview
+        textureRender.sharedMaterial.mainTexture = texture;
+        textureRender.transform.localScale = new Vector3(width, 1, height);
+    }
+}
